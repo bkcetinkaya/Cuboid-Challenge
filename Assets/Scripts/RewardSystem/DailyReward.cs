04dc72d3355412372caef05ab8b33ed5a0026c7a
Berke Kaan Çetinkaya <46684966+kaancetinkayasf@users.noreply.github.com>
2022-05-05 21:47:31 +0300

    Some parts of the code are refactored

--- Assets/Scripts/RewardSystem/DailyReward.cs
+++ Assets/Scripts/RewardSystem/DailyReward.cs
@@ -17,33 +17,46 @@ public class DailyReward : MonoBehaviour
     TimeSpan _result;
     double currCountdownValue ;
 
-    bool isRewardAvaliable;
-    
-
     private void Start()
+    {
+
+
+        if (!PlayerPrefs.HasKey("RewardClaimable"))
+        {
+            PlayerPrefs.SetInt("RewardClaimable", 1);
+            PlayerPrefs.Save();
+        }
+
+
+        if (PlayerPrefs.GetInt("RewardClaimable") == 0)
+        {
+            claimRewardButton.interactable = false;
+            
+            SetTimeDifference();
+            StartCoroutine(StartCountdown());
+        }
+        if (PlayerPrefs.GetInt("RewardClaimable") == 1)
+        {
+            
+            claimRewardButton.interactable = true;
+            StopCoroutine(StartCountdown());
+        }
+
+
+     
+
+    }
+
+    private void SetTimeDifference()
     {
         _result = CalculateTimeDifference();
         currCountdownValue = _result.TotalSeconds;
-     
-        
-
-        if(currCountdownValue > 0)
-        {
-            isRewardAvaliable = false;
-            StartCoroutine(StartCountdown());
-        }
-        if(currCountdownValue == 0)
-        {
-            isRewardAvaliable = true;
-        }
-        
-
     }
 
     private TimeSpan CalculateTimeDifference()
     {
         DateTime currentTime = DateTime.Now;
-        DateTime rewardTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 12, 00, 00);
+        DateTime rewardTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 21,42, 50);
 
 
         TimeSpan offsetTime = new TimeSpan(24, 00, 00);
@@ -51,6 +64,8 @@ public class DailyReward : MonoBehaviour
         TimeSpan diff = currentTime.Subtract(rewardTime);
 
         TimeSpan result = offsetTime - diff;
+
+        
 
         return result;
     }
@@ -64,9 +79,18 @@ public class DailyReward : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
             currCountdownValue--;
 
-            timeLeftText.text = TimeSpan.FromSeconds(currCountdownValue).ToString(@"dhh\:mm\:ss");
+
+            timeLeftText.text = TimeSpan.FromSeconds(currCountdownValue).ToString(@"hh\:mm\:ss");
         }
     }
 
+    private void Update()
+    {
 
+    }
+
+    public void ClaimReward()
+    {
+        
+    }
 }
